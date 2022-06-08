a193144e69b0b2ca801ccfac9dc4b3a86d7fd751
camila <cftinelli@outlook.com>
2022-06-08 17:57:19 -0300

    atualizando os arquivos

--- modulo01/aula02/variaveis.php
+++ modulo01/aula02/variaveis.php
@@ -1,7 +1,29 @@
 <?php
 
-$
+//camelCase
+$nomeCompletoDoAluno = "Josefer Climber Asufruino";
+$tenhoEmprego = true; //booleano
+$tenhoEmprego = false;
 
+$nome = "Camila"; //string
+$bebida = "cafe";
 
+$numeroCasa = 2006; //integer
+$cpf = "012.123.123-12"; //string
+$saldoBancario = -2569.89; //float
+
+$bebida = "agua";
+
+$bebida = "agua com gas";
+
+echo "Meu nome eh " .$nome. " e eu gosto de " .$bebida;
+
+//criando as variaveis
+$meuNome = "Camila Ferreira";
+$quantidadeIrmaos = 2;
+$nomeCachorro1 = "Leleco";
+$nomeCachorro2 = "Menino";
+$idade = 33;
+$anoNascimento = 1988;
 
 ?>
