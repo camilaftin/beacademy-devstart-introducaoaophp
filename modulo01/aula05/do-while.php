5f1d4bd2e9fdb41cd088f31f4c0ab009a895199f
camila <cftinelli@outlook.com>
2022-06-09 23:03:20 -0300

    arrays

---
+++ modulo01/aula05/do-while.php
@@ -0,0 +1,10 @@
+<?php
+
+$n = 1;
+
+do{
+
+  echo "Oh o pente!! <br>";
+  $n++;
+
+}while($n <= 10);
---
+++ modulo01/aula06/arrays.php
@@ -0,0 +1,17 @@
+<?php
+
+//modo antigo do php
+$aluno1 = array(
+  "zezim",
+  "leleco",
+  "maria"
+);
+
+//modo recente do php
+$arrayAlunos = [
+  "zezim",
+  "leleco",
+  "maria"
+];
+
+echo "<h1>Meu nome eh {$arrayAlunos[1]}";
